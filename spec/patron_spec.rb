9bc3fa9067be788ede2dd433521d5fafaab42eae
Zach Bergman <bzach634@gmail.com>
2024-04-08 12:51:22 -0600

    test: Add #initialize test with readable attributes

--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -1,7 +1,25 @@
-require './lib/patron'
+require "./lib/patron"
 
 RSpec.describe Patron do
     before(:each) do
         @patron_1 = Patron.new("Bob", 20)
+    end
+
+    describe "#initialize" do
+        it "exists" do
+            expect(@patron_1).to be_an_instance_of(Patron)
+        end
+
+        it "has a name" do
+            expect(@patron_1.name).to eq("Bob")
+        end
+
+        it "has spending money" do
+            expect(@patron_1.spending_money).to eq(20)
+        end
+
+        it "can store interests" do
+            expect(@patron_1.interests).to eq([])
+        end
     end
 end

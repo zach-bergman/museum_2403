a75e7231b1ae7a5c5334e274c7fa53aae9ba41a8
Zach Bergman <bzach634@gmail.com>
2024-04-09 06:50:51 -0600

    test: Add #recommend_exhibits test

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -29,7 +29,7 @@ RSpec.describe Patron do
     end
 
     describe "#add_exhibit" do
-        it 'can add exhibit Objects to the exhibits array' do
+        it "can add exhibit Objects to the exhibits array" do
             expect(@dmns.exhibits).to eq([])
 
             @dmns.add_exhibit(@gems_and_minerals)
@@ -37,6 +37,22 @@ RSpec.describe Patron do
             @dmns.add_exhibit(@imax)
 
             expect(@dmns.exhibits).to eq([@gems_and_minerals, @dead_sea_scrolls, @imax])
+        end
+    end
+
+    describe "#recommend_exhibits" do
+        it "returns an array of exhibit Objects that match a Patron's interests" do
+            @dmns.add_exhibit(@gems_and_minerals)
+            @dmns.add_exhibit(@dead_sea_scrolls)
+            @dmns.add_exhibit(@imax)
+
+            @patron_1.add_interest("Dead Sea Scrolls")
+            @patron_1.add_interest("Gems and Minerals")
+
+            @patron_2.add_interest("IMAX")
+
+            expect(@dmns.recommend_exhibits(@patron_1)).to eq([@dead_sea_scrolls, @gems_and_minerals])
+            expect(@dmns.recommend_exhibits(@patron_2)).to eq([@imax])
         end
     end
 end

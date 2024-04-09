573a43e698814f1b675b32bef08afbdeca956e19
Zach Bergman <bzach634@gmail.com>
2024-04-09 06:07:45 -0600

    feat: Declare Patron class/Add #initialize method with readable attributes

---
+++ lib/patron.rb
@@ -0,0 +1,11 @@
+class Patron
+    attr_reader :name,
+                :spending_money,
+                :interests
+
+    def initialize(name, spending_money)
+        @name = name
+        @spending_money = spending_money
+        @interests = []
+    end
+end

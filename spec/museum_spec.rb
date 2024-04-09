99a45eab5825acd53574e6563dcbc0693dc954f2
Zach Bergman <bzach634@gmail.com>
2024-04-09 06:34:34 -0600

    test: Add #add_exhibit test

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -27,5 +27,17 @@ RSpec.describe Patron do
             expect(@dmns.exhibits).to eq([])
         end
     end
+
+    describe "#add_exhibit" do
+        it 'can add exhibit Objects to the exhibits array' do
+            expect(@dmns.exhibits).to eq([])
+
+            @dmns.add_exhibit(@gems_and_minerals)
+            @dmns.add_exhibit(@dead_sea_scrolls)
+            @dmns.add_exhibit(@imax)
+
+            expect(@dmns.exhibits).to eq([@gems_and_minerals, @dead_sea_scrolls, @imax])
+        end
+    end
 end
 

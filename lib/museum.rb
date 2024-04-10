824f238fa52b92f0b85bb631b904233f4fff4ab7
Zach Bergman <bzach634@gmail.com>
2024-04-09 19:26:27 -0600

    feat: Add patrons instance variable and attr_reader

--- lib/museum.rb
+++ lib/museum.rb
@@ -1,9 +1,12 @@
 class Museum
-    attr_reader :name, :exhibits
+    attr_reader :name, 
+                :exhibits,
+                :patrons
 
     def initialize(name)
         @name = name
         @exhibits = []
+        @patrons = []
     end
 
     def add_exhibit(exhibit)

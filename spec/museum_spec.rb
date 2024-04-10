7599b77dd434dd8c25ad0046fb94488f96da8d6e
Zach Bergman <bzach634@gmail.com>
2024-04-09 19:43:16 -0600

    test: Add #patrons_by_exhibit_interest test

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -72,5 +72,30 @@ RSpec.describe Patron do
             expect(@dmns.patrons).to eq([@patron_1, @patron_2, @patron_3])
         end
     end
+
+    describe "#patrons_by_exhibit_interest" do
+        it "return a Hash with Exhibit Objects as keys and interested Patron Objects in Arrays as values" do
+            @dmns.add_exhibit(@gems_and_minerals)
+            @dmns.add_exhibit(@dead_sea_scrolls)
+            @dmns.add_exhibit(@imax)
+
+            @patron_1.add_interest("Gems and Minerals")
+            @patron_1.add_interest("Dead Sea Scrolls")
+            @patron_2.add_interest("Dead Sea Scrolls")
+            @patron_3.add_interest("Dead Sea Scrolls")
+
+            @dmns.admit(@patron_1)
+            @dmns.admit(@patron_2)
+            @dmns.admit(@patron_3)
+
+            expected = {
+                @gems_and_minerals => [@patron_1],
+                @dead_sea_scrolls => [@patron_1, @patron_2, @patron_3],
+                @imax => []
+            }
+
+            expect(@dmns.patrons_by_exhibit_interest).to eq(expected)
+        end
+    end
 end
 

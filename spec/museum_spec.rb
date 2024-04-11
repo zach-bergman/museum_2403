da8fc054ae756d9a0f2a0d228a8e60bb762bde05
Zach Bergman <bzach634@gmail.com>
2024-04-11 06:17:31 -0600

    refactor: Remove revenue readable attribue test

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -31,10 +31,6 @@ RSpec.describe Museum do
         it "can store patrons" do
             expect(@dmns.patrons).to eq([])
         end
-
-        it "can track revenue" do
-            expect(@dmns.revenue).to eq(0)
-        end
     end
 
     describe "#add_exhibit" do

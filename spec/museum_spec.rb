73f6c84916e511d3aaacb964fee39f1b1a502185
Zach Bergman <bzach634@gmail.com>
2024-04-10 20:27:16 -0600

    test: Add #not_enough_spending_money? test

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -221,20 +221,28 @@ RSpec.describe Museum do
         end
     end
 
-    describe "attend_higher_cost_exhibit_first" do
-        @dmns.add_exhibit(@gems_and_minerals)
-        @dmns.add_exhibit(@dead_sea_scrolls)
-        @dmns.add_exhibit(@imax)
+    # describe "attend_higher_cost_exhibit_first" do ## do next
+    #     @dmns.add_exhibit(@gems_and_minerals)
+    #     @dmns.add_exhibit(@dead_sea_scrolls)
+    #     @dmns.add_exhibit(@imax)
 
-        @patron_1.add_interest("Gems and Minerals")
-        @patron_1.add_interest("Dead Sea Scrolls")
-        @patron_1.add_interest("IMAX")
+    #     @patron_1.add_interest("Gems and Minerals")
+    #     @patron_1.add_interest("Dead Sea Scrolls")
+    #     @patron_1.add_interest("IMAX")
 
-        @dmns.admit(@patron_1)
+    #     @dmns.admit(@patron_1)
 
-        expect(@dmns.attend_higher_cost_exhibit_first(@patron_1)[0]).to eq(@imax)
-        expect(@dmns.attend_higher_cost_exhibit_first(@patron_1)[1]).to eq(@dead_sea_scrolls)
-        expect(@dmns.attend_higher_cost_exhibit_first(@patron_1)[2]).to eq(@gems_and_minerals)
+    #     expect(@dmns.attend_higher_cost_exhibit_first(@patron_1)[0]).to eq(@imax)
+    #     expect(@dmns.attend_higher_cost_exhibit_first(@patron_1)[1]).to eq(@dead_sea_scrolls)
+    #     expect(@dmns.attend_higher_cost_exhibit_first(@patron_1)[2]).to eq(@gems_and_minerals)
+    # end
+
+    describe "#not_enough_spending_money?" do
+        it "returns true if Patron does not have enough money to attend Exhibit" do
+            @dmns.add_exhibit(@dead_sea_scrolls)
+
+            expect(@dmns.not_enough_spending_money?(@patron_1)).to eq(true)
+        end
     end
 
 
